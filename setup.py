69a1785cb5ebac9f2ec94b066b50968b36c85bfa
Alexei Chklovski <chklovsk@aquarius01.ib0.hpc.qut.edu.au>
2025-02-09 15:37:57 +1000

    update setup.py with new version and model files

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='CheckM2',
-    version='1.0.2',
+    version='1.1.0',
     packages=find_packages(),
     data_files=[('data', ['checkm2/data/feature_ordering.json', 'checkm2/data/kegg_path_category_mapping.json',
                           'checkm2/data/min_ref_rsdata_v1.npz', 'checkm2/data/module_definitions.json']),
-                ('models', ['checkm2/models/specific_model_COMP.hd5', 'checkm2/models/cosine_table.pkl', 
+                ('models', ['checkm2/models/specific_model_COMP.keras', 'checkm2/models/cosine_table.pkl', 
                             'checkm2/models/model_CONT.gbm', 'checkm2/models/general_model_COMP.gbm', 'checkm2/models/scaler.sav']),
-                ('version', ['checkm2/version/diamond_path.json', 'checkm2/version/version_hashes_1.0.2.json']),
+                ('version', ['checkm2/version/diamond_path.json', 'checkm2/version/version_hashes_1.1.0.json']),
                 ('testrun', ['checkm2/testrun/TEST1.tst', 'checkm2/testrun/TEST2.tst', 'checkm2/testrun/TEST3.tst'])],
     include_package_data=True,
     url='https://github.com/chklovski/CheckM2',
